273fb317a5daf7a713a5c11eff331e4271bca311
Ilya Solomein <ilya.solomein@gmail.com>
2020-05-24 23:22:40 +0500

    rename, socket to yandex dns resolver

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 import socket
-from Parser.request_to_server import get_request
+from parser.request_to_server import get_request
 
 
 class Server:
     def __init__(self):
-        self.port = 8000
-        self.local = "127.0.0.1"
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((self.local, self.port))
+        self.port_listener = 8000
+        self.address_listener = "127.0.0.1"
+        self.socket_listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_listener.bind((self.address_listener, self.port_listener))
+        self.port_resolver = 53
+        self.address_resolver = "77.88.8.7"  # yandex family
+        self.socket_resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def start(self):
-        data, sender = self.socket.recvfrom(256)
+        data, sender = self.socket_listener.recvfrom(256)
         while data is not None:
             if data:
-                self.socket.sendto(b"d", sender)
+                self.socket_listener.sendto(b"d", sender)
                 request = get_request(data)
-                print(request)
-            data, sender = self.socket.recvfrom(256)
+
+            data, sender = self.socket_listener.recvfrom(256)
 
 
 s = Server()
